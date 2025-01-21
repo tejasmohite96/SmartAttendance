f6d4c335d15948ab414cc85571ded3e2fda14db8
tejasmohite96 <tejasmohite8819@gmail.com>
2025-01-21 16:08:19 +0530

    SmartAttendance Project Added

---
+++ SmartAttendance_API/src/main/java/com/tka/sams/api/SmartAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.tka.sams.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceApplication.class, args);
+	}
+
+}
